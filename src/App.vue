c893bc4a36f55e3b5eeff0c4a6b9b64076c299ef
zhouyu <zhouyu@trendytech.com.cn>
2018-12-27 10:24:54 +0800

    修改API DOM 演示

--- src/App.vue
+++ src/App.vue
@@ -3,61 +3,73 @@
     <ul class="container">
       <li>
         <h2>滚动条基础用法</h2>
-        <rudy-scrollbar width="400px" height="300px" :noresize="true">
-          <h3>内容不会变化的 建议设置noresize提高效率</h3>
-          <h4>因为自定义滚动条会监听内容区域大小，实时更新滚动条区域</h4>
+        <rudy-scrollbar width="600px" height="300px" :noresize="true">
+          <h3>内容宽度大于父级宽度时就会出现横向滚动条,
+            但是在这个例子中却没有出现横向滚动条，
+            因为例子中有图片，图片加载完成才会有图片
+            的宽度和高度出现，也就是这里图片还未加载
+            完成这个插件就已经初始化完成了，在
+            设置noresize="true" 之后是不监听内容区域大小
+            更新滚动条，所以推荐在img标签上设置宽高，下个例子中有用到。
+            原本还是推荐在内容区域不变化时使用noresize=“true”，elementUI推荐
+            这样可以优化很大性能。</h3>
+          <div>当然你也可以不设置noresize="true"，这样图片加载完成后，<br>
+          就会把父级撑开，监听容器大小的事件就会触发，从而更新滚动条插件<br>
+          第一第二个例子都设置了noresize属性，为了演示理解。</div>
           <div style="color: red;">使用Vue.use() 的方式注册滚动条，就使用<br>&lt;rudy-scrollbar&gt; 内容区 &lt;rudy-scrollbar&gt; 标签 </div>
           <ul class="description" style="height: 600px">
             <li class="title">属性：</li>
             <li>width <em>滚动条容器宽度</em> <span>String 例:100px/100em/10%</span></li>
             <li>height <em>滚动条容器高度</em> <span>String 例:100px/100em/10%</span></li>
-            <li>max-width <em>滚动条容器最大宽度</em> <span>同上</span></li>
-            <li>max-height <em>滚动条容器最大高度</em> <span>同上</span></li>
+            <img src="./images/oneone.jpg" alt="">
           </ul>
         </rudy-scrollbar>
       </li>
       <li>
-        <h2>横向与纵向滚动条</h2>
-        <rudy-scrollbar width="400px" height="300px">
-          <h3>内容宽度大于父级宽度时就会出现横向滚动条</h3>
-          <ul class="description" style="height: 600px; width: 1000px">
+        <h2>最大高度，最大宽度</h2>
+        <rudy-scrollbar max-width="600px" max-height="300px" :noresize="true">
+          <h3>因一些特殊场景需要设置最大宽高</h3>
+          <ul class="description" style="height: 600px;">
             <li class="title">属性：</li>
-            <li>width <em>滚动条容器宽度</em> <span>String 例:100px/100em/10%</span></li>
-            <li>height <em>滚动条容器高度</em> <span>String 例:100px/100em/10%</span></li>
-            <li>max-width <em>滚动条容器最大宽度</em> <span>同上</span></li>
-            <li>max-height <em>滚动条容器最大高度</em> <span>同上</span></li>
+            <li>max-width <em>滚动条容器最大宽度</em> <span>String 例:100px/100em/10%</span></li>
+            <li>max-height <em>滚动条容器最大高度</em> <span>String 例:100px/100em/10%</span></li>
+            <img src="./images/twotwo.jpg" alt="" style="width: 1000px;height:auto">
           </ul>
         </rudy-scrollbar>
       </li>
       <li>
         <h2>支持过渡滚动效果</h2>
-        <rudy-scrollbar width="400px" height="300px" :transition="true">
+        <rudy-scrollbar width="600px" height="300px" :transition="true">
           <h3>点击滚动条透明区域，查看效果</h3>
-          <ul class="description" style="height: 600px; width: 1000px">
+          <ul class="description" style="height: 600px">
             <li class="title">属性：</li>
             <li>transition <em>滚动条过渡</em> <span>Boolean 缺省：false</span></li>
+            <img src="./images/threethree.jpg">
           </ul>
         </rudy-scrollbar>
       </li>
       <li>
         <h2>支持固定显示滚动条</h2>
-        <rudy-scrollbar width="400px" height="300px" :lastingShowBar="true" :transition="true">
+        <rudy-scrollbar width="600px" height="300px" :lastingShowBar="true" :transition="true">
           <h3>查看滚动条效果</h3>
           <ul class="description" style="height: 600px; width: 1000px">
             <li class="title">属性：</li>
             <li>lastingShowBar <em>是否持久显示滚动条</em> <span>Boolean 缺省：false</span></li>
+            <img src="./images/fourfour.jpg">
           </ul>
         </rudy-scrollbar>
       </li>
       <li>
         <h2>支持滚动条定位</h2>
-        <scrollbar ref="scrollbar" width="400px" height="300px" :transition="true">
+        <scrollbar ref="scrollbar" width="600px" height="300px" :transition="true">
           <h3>输入x与y轴坐标，跳转。非法字符忽略</h3>
           <ul class="description" style="height: 600px; width: 1000px">
             <li class="title">方法：</li>
             <li>scrollTop <em>上下滚动</em> <span>Number 例:100</span></li>
             <li>scrollLeft <em>垂直滚动</em> <span>Number 例:100</span></li>
             <li>scrollTo <em>上下垂直滚动</em> <span>Number 例：x:100,y:100</span></li>
+            <img src="./images/five1.jpg">
+            <img src="./images/five2.jpg">
           </ul>
         </scrollbar>
         <hr>  
@@ -69,47 +81,26 @@
       </li>
       <li>
         <h2>支持滚动回调</h2>
-        <rudy-scrollbar width="400px" height="300px" :transition="true" @scroll="scrollHandle">
+        <rudy-scrollbar width="600px" height="300px" :transition="true" @scroll="scrollHandle">
           <h3>滚动查看位置（打开控制台查看返回事件对象，此事件对象为原生滚动事件对象）</h3>
           <span style="backround-color: #ffffff; position: absolute; right: 10px; top: 5px; color: red;">x:{{this.x}} y:{{this.y}}</span>
           <ul class="description" style="height: 600px; width: 1000px;">
-            <li class="title">方法：</li>
+            <li class="title">事件：</li>
             <li>scroll <em>滚动事件</em> <span>返回值 event 滚动事件对象</span></li>
+            <img src="./images/six1.jpg" alt="">
+            <img src="./images/six2.jpg" alt="">
           </ul>
         </rudy-scrollbar>
       </li>
       <li>
         <h2>其他属性</h2>
-        <rudy-scrollbar width="400px" height="300px" :transition="true" @scroll="scrollHandle">
+        <rudy-scrollbar width="600px" height="300px" :transition="true">
           <h3>滚动条所有对外暴露属性</h3>
           <ul class="description" style="height: 600px; width: 1000px;">
-            <li class="title">方法：</li>
+            <li class="title">属性</li>
             <li>
-<pre>
-  props: {
-    native: Boolean,
-    wrapStyle: {}, // 滚动条外框样式
-    wrapClass: {}, // 滚动条外框class
-    viewClass: {}, // 内容区域class
-    viewStyle: {}, // 内容区域样式
-    barClass: {}, // 滚动条class
-    thumbClass: {}, // 滚动条拖动按钮class
-    lastingShowBar: Boolean, // 是否持续显示滚动条
-    transition: Boolean, // 滚动是否添加过渡效果
-    noresize: Boolean, // 如果 container 尺寸不会发生变化，最好设置它可以优化性能
-    width: String, 
-    height: String, 
-    maxHeight: String,
-    maxWidth: String,
-    tag: {
-      type: String,
-      default: "div"
-    }
-  }
-  内部属性wrap 可访问
-  直接获取滚动DOM
-  可通过滚动的Dom 获取 scrollTop、scrollLeft 等等信息
-</pre>
+              <img src="./images/seven1.jpg" alt="">
+              <img src="./images/seven2.jpg" alt="">
             </li>
           </ul>
         </rudy-scrollbar>
@@ -132,6 +123,8 @@
 <script>
 
 import scrollbar from '../libs/scrollbar/index.js'
+
+// 图片引入
 
 export default {
   name: 'app',
@@ -163,14 +156,14 @@ export default {
   height 100%
   min-width 1200px
   > li
-    width calc(100% / 3)
+    width calc(100% / 2)
     float left
     height 400px
     h2
       text-align center
     h3
     h4
-      width 280px
+      width 550px
       margin 5px
   .el-scrollbar
     background-color #ffffff
